1fde9ca304678367e426beee2a5e919a582c74a1
Simas G <simas@simson.lt>
2025-01-28 18:22:34 +0200

    mokyklu filtras

--- components/FilterSchools.jsx
+++ components/FilterSchools.jsx
@@ -11,17 +11,21 @@ const FilterSchools = ({ regions, onFilter }) => {
 
   return (
     <div className="lg:max-w-screen-lg w-full m-auto p-10">
-
-      <div className="flex flex-col">
+      <div className="relative mb-10 w-full flex  items-center justify-between rounded-md">
+      <svg className="absolute left-2 block h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <circle cx="11" cy="11" r="8" className=""></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65" className=""></line>
+      </svg>
+      <input type="name" name="search" className="h-12 w-full cursor-text rounded-md border border-gray-100 bg-gray-100 py-4 pr-15 pl-12 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" placeholder="Ieškokite mokyklos" />
+    </div>
+      <div className="collapse bg-white border border-primary collapse-arrow ">
+        <input type="checkbox" />
+        <h3 className="collapse-title text-primary">Filtras</h3>
+        
+        <div className="flex flex-col collapse-content ">
         <div className="rounded-xl border border-gray-200 bg-white p-6 shadow-lg">
           <form className="">
-            <div className="relative mb-10 w-full flex  items-center justify-between rounded-md">
-              <svg className="absolute left-2 block h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <circle cx="11" cy="11" r="8" className=""></circle>
-                <line x1="21" y1="21" x2="16.65" y2="16.65" className=""></line>
-              </svg>
-              <input type="name" name="search" className="h-12 w-full cursor-text rounded-md border border-gray-100 bg-gray-100 py-4 pr-40 pl-12 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" placeholder="Ieškokite mokyklos" />
-            </div>
+            
 
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <div className="flex flex-col">
@@ -65,6 +69,9 @@ const FilterSchools = ({ regions, onFilter }) => {
           </form>
         </div>
       </div> 
+      </div>
+      
+      
   </div>
 
   );
