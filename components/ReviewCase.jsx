86f4dc7e124e477de7803b16b2ac95f879e19e21
Simas G <simas@simson.lt>
2025-02-16 13:59:52 +0200

    reviewCase design

--- components/ReviewCase.jsx
+++ components/ReviewCase.jsx
@@ -1,24 +1,51 @@
 import StarRating from "./StarRating"
 const ReviewCase = ({review}) => {
-    const {criterion1, criterion2, criterion3, user, comment, createdAt} = review
-    const rating = (criterion1 + criterion2 + criterion3) / 3
+    const {criterion1, criterion2, criterion3, user, comment, createdAt, rec} = review
+    const rating = (criterion1 + criterion2 + criterion3) / 3;
+    const date = new Date(createdAt);
+    const formatedDate = date.toISOString().split("T")[0];
   return (
     review.comment ? (
-        <div className="mx-10 max-w-64 min-h-20 border-2 border-black rounded-xl">
-            <div>
-                <StarRating r={rating}/>
+        <div className="mx-10 max-w-72 max-h-48 border border-black rounded-xl px-4 flex flex-col pb-3">
+            <div className="flex items-center justify-between pt-3">
+                <div>
+                    <StarRating r={rating}/>
+                </div>
+                <div className="text-gray-400">
+                    {formatedDate}
+                </div>
             </div>
-            {comment}
+            <div className="pt-2 pb-4">
+                <p className="line-clamp-3">{comment}</p>
+            </div>
+            <div className="w-full flex justify-end">
+                <button className="px-5 py-2 btn btn-primary btn-outline">Išsamiau</button>
+            </div>
+            
         </div>
 
     ) : (
-        <div>
-
-            <div className="mx-10 max-w-64 min-h-20 border-2 border-black rounded-xl">
-                <StarRating r={rating}/>
+        <div className="mx-10 max-w-64 min-h-20 border border-black rounded-xl px-4">
+            <div className="flex items-center justify-between pt-3">
+                <div>
+                    <StarRating r={rating}/>
+                </div>
+                {rec === true ? (<div className="w-7 h-7">
+                    <img src="/images/thumbs-up.svg" alt="" />
+                </div>) : (<div className="w-7 h-7 pt-1">
+                    <img src="/images/thumbs-down-red.svg" alt="" />
+                </div>)}
             </div>
-        
-        </div>)
+            <div className="pt-6 pb-4 flex justify-between items-center">
+                <p className="text-gray-400">
+                    {formatedDate}
+                </p>
+                <div className="">
+                    <button className="px-5 py-2 btn btn-primary btn-outline">Išsamiau</button>
+                </div>
+            </div>
+        </div>
+    )
   )
 }
 
--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -1,62 +1,28 @@
 import React from "react";
 
-const StarRating = ({ r = 0, size = "sm", number = undefined}) => {
-  const starCount = 5; // Total number of stars
+const StarRating = ({ r = 0, size = "sm", number = undefined }) => {
+  const fullStars = Math.floor(r);
+  const halfStars = Math.round(r) - fullStars;
+  const emptyStars = 5 - fullStars - halfStars;
 
-  // Determine the full, half, and empty stars
-  const fullStars = Math.floor(r); // Full stars count
-  const halfStar = r % 1 >= 0.5; // Check if there's a half star
+  const responsiveSize = size === "xl" ? "md:w-7 md:h-7 w-5 h-5" : "w-5 h-5";
 
   return (
-    <div className="rating rating-xl py-1 gap-1 items-center">
-      {/* Render full stars */}
-      {size === "xl" ? (<div className='px-1 text-lg md:text-2xl text-gray-400 font-title'>
+    <div className="flex gap-1 items-center">
+      <div className={`pr-1 text-gray-400 font-title ${size === "xl" ? "md:text-2xl text-lg" : "text-lg"}`}>
         {r.toFixed(1)}
-      </div>) : (<div className='px-1 text-md font-title text-gray-400'>
-        {r.toFixed(1)}
-      </div>)}
-      
-      {Array.from({ length: fullStars }).map((_, index) => (
-        size === "xl" ? (<input
-          key={`full-${index}`}
-          type="radio"
-          name="rating"
-          className="mask mask-star-2 bg-orange-400 h-5 w-5 md:w-7 md:h-7"
-          defaultChecked
-          disabled
-        />) : (<input
-          key={`full-${index}`}
-          type="radio"
-          name="rating"
-          className="mask mask-star-2 bg-orange-400"
-          defaultChecked
-          disabled
-        />)
-      ))}
-      
-      {/* Render half star */}
-      {halfStar && (
-        <input
-          type="radio"
-          name="rating"
-          className="mask h-5 w-5 md:h-7 md:w-7 mask-star-2 bg-orange-400 opacity-50"
-          defaultChecked
-          disabled
-        />
-      )}
-
-      {/* Render empty stars */}
-      {Array.from({ length: starCount - fullStars - (halfStar ? 1 : 0) }).map((_, index) => (
-        <input
-          key={`empty-${index}`}
-          type="radio"
-          name="rating"
-          className="mask mask-star-2 bg-gray-300 h-5 w-5 md:h-7 md:w-7"
-          disabled
-        />
-      ))}
-      {number !== undefined && <div className="text-gray-400 text-lg md:text-xl">({number})</div>}
-      
+      </div>
+      <div className="flex">
+        {[...Array(fullStars)].map((_, index) => (
+          <div key={`full-${index}`} className={`mask mask-star-2 bg-orange-400 ${responsiveSize}`}></div>
+        ))}
+        {halfStars === 1 && (
+          <div key="half" className={`mask mask-star-2 bg-orange-400 opacity-45 ${responsiveSize}`}></div>
+        )}
+        {[...Array(emptyStars)].map((_, index) => (
+          <div key={`empty-${index}`} className={`mask mask-star-2 bg-gray-300 ${responsiveSize}`}></div>
+        ))}
+      </div>
     </div>
   );
 };
