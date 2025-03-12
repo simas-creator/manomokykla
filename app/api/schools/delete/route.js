2f06eb9f4c40b47882b08546a378ce847840dc69
Simas G <simas@simson.lt>
2025-03-12 17:45:45 +0200

    bug fix

--- app/api/schools/delete/route.js
+++ app/api/schools/delete/route.js
@@ -1,6 +1,6 @@
 import School from '@/lib/modals/school'
 import { NextResponse } from 'next/server';
-export default async function DELETE(req){
+export async function DELETE(req){
     const {searchParams} = new URL(req.url)
     const n = searchParams.get('n');
     await School.findOneAndDelete({n})
