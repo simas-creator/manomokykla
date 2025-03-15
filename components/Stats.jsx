dea16b6fdadf94e4974b11f8d435e7c0b5290ab8
Simas G <simas@simson.lt>
2025-03-15 22:04:25 +0200

    faster document count

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -4,10 +4,10 @@ import Review from '@/lib/modals/review'
 import User from '@/lib/modals/user'
 
 const getStats = async () => {
-    const schoolsPromise = School.countDocuments();
-    const teachersPromise = Teacher.countDocuments();
-    const reviewsPromise = Review.countDocuments();
-    const userPromise = User.countDocuments();
+    const schoolsPromise = School.estimatedDocumentCountd();
+    const teachersPromise = Teacher.estimatedDocumentCountd();
+    const reviewsPromise = Review.estimatedDocumentCountd();
+    const userPromise = User.estimatedDocumentCountd();
     const [schools, teachers, reviews, users] = await Promise.all([schoolsPromise, teachersPromise, reviewsPromise, userPromise])
     return {
         schools,
